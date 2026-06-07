c9ec942f3821df30697383cfdf15c0568d48fa21
xiexiyang <sdlgxxy@gmail.com>
2026-06-07 17:48:40 +0800

    feat: 添加请求日志中间件

--- server/index.mjs
+++ server/index.mjs
@@ -39,6 +39,10 @@ const PORT = 3001;
 
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`[${new Date().toLocaleTimeString()}] ${req.method} ${req.url}`);
+  next();
+});
 
 // 进行中的分析请求，用于取消
 const activeAnalyses = new Map();
