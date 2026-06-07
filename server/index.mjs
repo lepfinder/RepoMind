0d545ea9ad5def11ffea603c9b80fddcd4e84ffa
xiexiyang <sdlgxxy@gmail.com>
2026-06-07 18:05:32 +0800

    revert: 恢复 dev 脚本和日志方式

--- server/index.mjs
+++ server/index.mjs
@@ -40,7 +40,7 @@ const PORT = 3001;
 app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
-  process.stdout.write(`[${new Date().toLocaleTimeString()}] ${req.method} ${req.url}\n`);
+  console.log(`[${new Date().toLocaleTimeString()}] ${req.method} ${req.url}`);
   next();
 });
 
